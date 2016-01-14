2ee0dc220795398e515ac184c0752ef069eca356
DocEon <aerozep@gmail.com>
2016-01-14 18:06:16 -0500

    Create books.php

---
+++ books.php
@@ -0,0 +1,46 @@
+{
+  "A Man for All Seasons" : {
+    "Author" : "Bolt, Robert",
+    "Pages" : 157
+  },
+  "Brecht on Theater" : {
+    "Author" : "Brecht, Bertolt",
+    "Hardcover" : "True",
+    "Pages" : 294,
+    "Translator" : "Willett, John"
+  },
+  "Krapp's Last Tape" : {
+    "Author" : "Beckett, Samuel",
+    "Pages" : 145
+  },
+  "Parables for Theater" : {
+    "Author" : "Brecht, Bertolt",
+    "Pages" : 188,
+    "Translator" : "Bentley, Eric; Apelman, Maja"
+  },
+  "The Complete Dramatic Works" : {
+    "Author" : "Beckett, Samuel",
+    "Pages" : 476
+  },
+  "The Day Room" : {
+    "Author" : "DeLillo, Don",
+    "Pages" : 102
+  },
+  "The Good Person of Szechwan" : {
+    "Author" : "Brecht, Bertolt",
+    "Pages" : 156,
+    "Translator" : "Willett, John"
+  },
+  "Three Novels: Molloy, Malone Dies, The Unnameable" : {
+    "Author" : "Beckett, Samuel",
+    "Pages" : 400
+  },
+  "Tiny Alice" : {
+    "Author" : "Albee, Edward",
+    "Pages" : 180
+  },
+  "Who's Afraid of Virginia Woolf" : {
+    "Author" : "Albee, Edward",
+    "Pages" : 284
+  }
+}
